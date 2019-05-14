ccc5b116f8eee54bebb38881759144236e634ed3
Antoni Kepinski <xxczaki@pm.me>
2019-05-14 22:03:25 +0200

    Install the latest Node.js, better logger

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,76 +6,85 @@ cyan=$(tput setaf 6)
 normal=$(tput sgr0)
 
 # Welcome message
-echo "Welcome! Before you start, please make sure that:"
-echo "- You are using Linux Mint (this script might also work on Ubuntu, but it has not been tested there)"
-echo "- You checked the configuration files and adjusted them to your needs"
+echo
+cat << "EOF"
+|  _  \    | |      
+| | | |___ | |_ ___ 
+| | | / _ \| __/ __|
+| |/ / (_) | |_\__ \
+|___/ \___/ \__|___/  
+EOF
+echo
+echo "Welcome!"
+echo "This script will install xxczaki's dotfiles on your machine"
+echo "More information here: https://github.com/xxczaki/dots"
 echo
 
 # Simple prompt
 read -p "Ready to start? (y/n)? " CONT
 if [ "$CONT" = 'y' ]; then
-        printf "%40s\n" "${cyan}› Installing necessary packages...${normal}"
+        echo
+        printf "${cyan}› Warming up...${normal}"
+        echo
 
-        sudo apt-get update > /dev/null 2>&1
-        sudo apt install zsh curl nodejs npm git > /dev/null 2>&1
+        sudo apt update -y > /dev/null 2>&1
+        sudo apt install -y zsh curl git > /dev/null 2>&1
 
-        printf "%40s\n" "${cyan}› Verifying zsh installation...${normal}"
+        curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - > /dev/null 2>&1
 
+        echo
+        printf "${cyan}› Configuring zsh${normal}"
+        echo
 
-        if zsh --version | grep -q 'zsh'; then
-            printf "%40s\n" "${green}✔︎ zsh successfully installed${normal}"
-        fi
-
-        printf "%40s\n" "${cyan}› Making zsh a default shell...${normal}"
+        printf "${cyan} › Making zsh a default shell...${normal}"
 
         chsh -s $(which zsh)
+        echo
 
-        printf "%40s\n" "${cyan}› Checking if zsh is a default shell...${normal}"
+        printf "${cyan} › Copying zsh configuration file...${normal}"
 
-        if echo $SHELL | grep -q 'zsh'; then
-            printf "%40s\n" "${green}✔ ︎zsh is now a default shell${normal}"
-        fi
+        cp -f assets/.zshrc ~/ > /dev/null 2>&1
+        echo
 
-        printf "%40s\n" "${cyan}› Installing oh-my-zsh...${normal}"
+        printf "${cyan} › Installing oh-my-zsh...${normal}"
 
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" > /dev/null 2>&1
+        echo
 
-        printf "%40s\n" "${cyan}› Copying zsh configuration file...${normal}"
-
-        cp -f .zshrc ~/ > /dev/null 2>&1
-
-        printf "%40s\n" "${cyan}› Checking if zsh configuration file was copied...${normal}"
-
-        if [ -e "~/.zshrc" ]; then
-            printf "%40s\n" "${green}✔ ︎zsh configuration was copied successfully${normal}"
-        fi 
-
-        printf "%40s\n" "${cyan}› Installing zsh-syntax-highlighting plugin...${normal}"
+        printf "${cyan} › Installing zsh-syntax-highlighting plugin...${normal}"
 
         git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting > /dev/null 2>&1
+        echo
 
-        printf "%40s\n" "${cyan}› Verifying npm installation...${normal}"
-
-        if npm --help | grep -q 'doctor'; then
-            printf "%40s\n" "${green}✔︎ npm successfully installed${normal}"
-        fi
-
-        printf "%40s\n" "${cyan}› Installing pure prompt...${normal}"
+        printf "${cyan} › Installing pure prompt...${normal}"
 
         npm install --global pure-prompt > /dev/null 2>&1
+        echo
 
-        printf "%40s\n" "${cyan}› Copying npm configuration file & making a directory for packages...${normal}"
+        printf "${green}✔︎ Finished zsh configuration!${normal}"
+        echo
+
+        echo
+        printf "${cyan}› Configuring Node.js environment...${normal}"
+        echo
+
+        printf "${cyan} › Installing Node.js & npm...${normal}"
+
+        sudo apt install -y nodejs > /dev/null 2>&1
+        echo
+
+        printf "${cyan} › Making a directory for packages...${normal}"
 
         mkdir ~/.npm-packages > /dev/null 2>&1
-        cp -f .npmrc ~/ > /dev/null 2>&1
+        echo
 
-        printf "%40s\n" "${cyan}› Checking if npm configuration file was copied...${normal}"
+        printf "${cyan} › Copying npm configuration file${normal}"
 
-        if [ -e "~/.npmrc" ]; then
-            printf "%40s\n" "${green}✔ ︎zsh configuration was copied successfully${normal}"
-        fi 
+        cp -f assets/.npmrc ~/ > /dev/null 2>&1
+        echo
         
-        printf "%40s\n" "${green}✔︎ Done!${normal}"
+        printf "${green}✔︎ Done!${normal}"
+        echo
 else
   echo "› Exiting";
 fi
